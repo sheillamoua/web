d36eeb72b661b6b4f16e1612aec1a6f84fc67eb7
sheillamoua <smoua300@gmail.com>
2026-02-24 18:23:48 +0000

    Tue, Feb 24, 2026, 10:23 AM -08:00

--- script/loading.js
+++ script/loading.js
@@ -3,13 +3,17 @@ bg = document.querySelector('.bg')
 
 load = 0
 
-int = setInterval(blurring, 30)
+int = setInterval(blurring, 40)
 
 function blurring() {
     load = load + 1
     loadText.innerText = `${load}%`
     loadText.style.opacity = 1-load/100
     bg.style.filter = `blur(${50-load/2}px)`
+
+    if(load > 99) {
+        clearIntveral(int)
+    }
 }
 
 
