5425add10a8a58579f86620ffe83c63b00bd0b5d
sheillamoua <smoua300@gmail.com>
2026-02-25 17:55:24 +0000

    Wed, Feb 25, 2026, 9:55 AM -08:00

--- script/supreme-space-robot.js
+++ script/supreme-space-robot.js
@@ -1,7 +1,19 @@
 square = document.querySelector('.square')
 
 size = 50
+direction = "bigger"
+
+int = setInterval(Size, 30)
 
 function Size () {
-    size
+    if(size = 50) {
+        direction = "bigger"
+    }
+    if(size > 50) {
+        direction = "smaller"
+    }
+    if(direction == bigger) {
+        size = size + 1
+    }
+    if(direction == smaller)
 }
