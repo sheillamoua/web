d8746b91363544ed0f827ffdb1f968320b0381aa
sheillamoua <smoua300@gmail.com>
2026-03-04 18:21:45 +0000

    Wed, Mar 4, 2026, 10:21 AM -08:00

--- script/event.js
+++ script/event.js
@@ -43,7 +43,9 @@ hover.addEventListener('mouseleave', () =>{
 })
 
 dblclick.addEventListener("dblclick", () => {
-    dblclick.style.opacity = 0
+    load = 0
+    load = load + 1
+    dblclick.style.opacity = 1-load/100
 
 })
 
