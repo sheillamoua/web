c05e07e9f2c9a3dc6fe8b08a5d5af0da7557085f
sheillamoua <smoua300@gmail.com>
2026-03-09 16:37:57 +0000

    Mon, Mar 9, 2026, 9:37 AM -07:00

--- script/colors.js
+++ script/colors.js
@@ -5,5 +5,8 @@ colors = ['red','orange','yellow','green','blue','purple','pink','white']
 index = 0
 
 box.addEventListener('click', () => {
-    
+    click.style.background = colors[index]
+    index = index + 1
+    if (index == colors.length){
+        index = 0
 })
