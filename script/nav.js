854bd9beaa08e798f2871776cb8c14e0ff732113
sheillamoua <smoua300@gmail.com>
2026-03-20 16:23:29 +0000

    Fri, Mar 20, 2026, 9:23 AM -07:00

---
+++ script/nav.js
@@ -0,0 +1,10 @@
+nav = document.querySelector('.nav')
+
+window.addEventListener('scroll', () => {
+    if(scrollY > 500) {
+        nav.classList.add('active')
+    }
+    else {
+        nav.classList.remove('active')
+    }
+})
