edf67ee0b6a25a092f040056bc43f4ac6e6e9312
sheillamoua <smoua300@gmail.com>
2026-03-23 17:06:54 +0000

    Mon, Mar 23, 2026, 10:06 AM -07:00

--- script/insect.js
+++ script/insect.js
@@ -24,6 +24,7 @@ for (let i = 0; i < choose_insect_btns.length; i = i + 1)
 
 function startGame() {
     setTimeout(createInsect, 1000)
+    setInterval(increaseTime, 1000)
 }
 
 function createInsect() {
