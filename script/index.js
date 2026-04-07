5fe07a30dbf509741a2a4b2e83478d51192b367d
sheillamoua <smoua300@gmail.com>
2026-04-07 04:14:54 +0000

    Mon, Apr 6, 2026, 9:14 PM -07:00

---
+++ script/index.js
@@ -0,0 +1,11 @@
+hover = document.querySelector('.menu a')
+
+hover.addEventListener('mouseenter', () =>{
+    hover.style.height = "40px"
+    hover.style.width = "100px"
+})
+
+hover.addEventListener('mouseleave', () =>{
+    hover.style.height = "20px"
+    hover.style.width = "250px"
+})
