7a405fe2537ce48ec5e12d69daa6db5bd5d0ada3
sheillamoua <smoua300@gmail.com>
2026-04-07 04:18:14 +0000

    Mon, Apr 6, 2026, 9:18 PM -07:00

--- script/index.js
+++ script/index.js
@@ -4,7 +4,7 @@ hover.addEventListener('mouseenter', () =>{
     hover.style.height = "40px"
     hover.style.width = "100px"
 })
-
+consloe.log = 'hello'
 hover.addEventListener('mouseleave', () =>{
     hover.style.height = "20px"
     hover.style.width = "250px"
